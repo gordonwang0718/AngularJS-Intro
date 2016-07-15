4ed5aff8b43c1bfe94e578303b9eea60ae106ee6
gordonwang0718 <Gordon@XuyangdeMacBook-Pro.local>
2016-07-15 16:35:41 -0700

    step 7 - 6 Directive and Views - ng-model

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 // Code goes here
 
-angular.module('app',[]);
+angular.module('app', []);
 angular.module('app').controller('MainController', function($scope, $http) {
-	
-	var onUserComplete = function(response) {
-	  $scope.user = response.data;
-	};
-	
-	var onError = function(reason){
-	  $scope.error = "could not fetch the user";
-	}
-	
-	$http.get("https://api.github.com/users/robconery")
-	  .then(onUserComplete, onError);
-	
-	$scope.message = "hello Gordon";
-// 	$scope.person = {
-// 	  firstName:"Gordon",
-// 	  lastName:"Wang",
-// 	  imageSrc:"http://s5.sinaimg.cn/mw690/005y1m2Ozy70VL5R2KM64&690"
-// 	};
-  
+
+  var onUserComplete = function(response) {
+    $scope.user = response.data;
+  };
+
+  var onError = function(reason) {
+    $scope.error = "could not fetch the user";
+  }
+
+  $http.get("https://api.github.com/users/Angular")
+    .then(onUserComplete, onError);
+
+  $scope.message = "hello Gordon";
+  $scope.username = "angular";
+  // 	$scope.person = {
+  // 	  firstName:"Gordon",
+  // 	  lastName:"Wang",
+  // 	  imageSrc:"http://s5.sinaimg.cn/mw690/005y1m2Ozy70VL5R2KM64&690"
+  // 	};
+
 });
 
 //$http.get is going to return a promise, so it cannot get details about robconery 
