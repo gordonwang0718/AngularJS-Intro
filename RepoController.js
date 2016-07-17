c71dc7451e49298e89903f7f1432407f0b651b25
gordonwang0718 <Gordon@XuyangdeMacBook-Pro.local>
2016-07-17 02:57:16 -0700

    step 20 - Routing - Repo advanced operation

---
+++ RepoController.js
@@ -0,0 +1,24 @@
+// Code goes here
+(function() {
+
+    var app = angular.module("app");
+    
+    var RepoController = function($scope, $routeParams, github) {
+      
+      var onRepo = function(data){
+        $scope.repo = data;
+      };
+      
+      var onError = function(reason){
+        $scope.error = reason;
+      };
+      
+      var reponame = $routeParams.reponame;
+      var username = $routeParams.username;
+      
+      github.getRepoDetails(username, reponame)
+            .then(onRepo, onError);
+    };
+    
+    app.controller("RepoController", RepoController);
+}());
